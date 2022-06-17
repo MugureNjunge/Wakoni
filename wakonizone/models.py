544c7a775d5b44488a2bd38022356902c6a2d622
MugureNjunge <maureennjunge22@gmail.com>
2022-06-17 12:46:28 +0300

    create profile model

--- wakonizone/models.py
+++ wakonizone/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    profile_pic= CloudinaryField('image')
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True)
+    fullname = models.CharField(max_length=100,blank=True)
+    locality = models.CharField(max_length=200, blank=True, null=True)
+    locale_lane = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.fullname
+
+    @classmethod
+    def save_profile(self):
+      self.save() 
+      
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    def delete_profile(self):
+         self.delete()
+
+  
+
